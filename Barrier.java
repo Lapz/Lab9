2f112c7296c9d929c0e6991cc4ae75df8c4aacaf
Lenard Pratt <l3np27@gmail.com>
2020-04-03 14:01:26 +0100

    Part 1

---
+++ Barrier.java
@@ -0,0 +1,49 @@
+
+/**
+ * Lab 9: Thread Barrier Barrier Class
+ *
+ * Processes join the barrier and are held until barrierSize processes have
+ * joined.
+ *
+ *
+ */
+public class Barrier {
+
+    /**
+     * Size of the barrier, which is the minimum number of processes to proceed.
+     */
+    private int barrierSize;
+    private static int parked = 0;
+
+    /**
+     * Create a barrier of a given size
+     *
+     * @param size
+     */
+    public Barrier(int size) {
+        barrierSize = size;
+        System.out.println("Barrier size = " + barrierSize);
+    }
+
+    /**
+     * Processes join at barrier and either wait or are allowed past.
+     *
+     * @param p The process joining
+     * @throws java.lang.InterruptedException
+     */
+    public void joinBarrier(Process p) throws InterruptedException {
+        System.out.println(p.getName() + " waiting on barrier");
+        parked += 1;
+
+        while (parked <= barrierSize) {
+            // park the threads
+        }
+
+        synchronized (this) {
+            notifyAll();
+        }
+
+        // add code here
+        System.out.println(p.getName() + " passed the barrier");
+    }
+}
